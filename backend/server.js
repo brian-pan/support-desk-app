90be96350a7a75d06d6bd827e0bc4de59d419acc
Brian Pan <brianpanzifan@gmail.com>
2022-04-13 19:36:03 -0400

    auth middleware done

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 const { errorHandler } = require("./middleware/errorMiddleware");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 //connect MongoDB
 const connectDB = require("./config/db");
 connectDB();
+// mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+//   useNewUrlParser: true,
+//   // useCreateIndex: true,
+//   useUnifiedTopology: true,
+//   // useFindAndModify: false,
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//   console.log("Database connected");
+// });
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
@@ -27,16 +40,3 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}:`);
 });
-
-// mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//     console.log("Database connected");
-// });
