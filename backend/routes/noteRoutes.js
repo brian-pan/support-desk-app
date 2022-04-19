8da4a98fe6e6d96721753e4cef8ce065c68b251c
Brian Pan <brianpanzifan@gmail.com>
2022-04-19 08:05:16 -0400

    backend added create note fn

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const { getNotes } = require("../controllers/noteController");
 const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(protect, getNotes);
+router.route("/").get(protect, getNotes).post(protect, addNote);
 
 module.exports = router;
 
