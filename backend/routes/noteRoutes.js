f2b1fc10046038b64fead72337162925a1f3e5c5
Brian Pan <brianpanzifan@gmail.com>
2022-04-20 00:09:09 -0400

    bug fixed

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getNotes } = require("../controllers/noteController");
+const { getNotes, addNote } = require("../controllers/noteController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getNotes).post(protect, addNote);
